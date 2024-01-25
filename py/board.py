8516f1039deb7d7a93717f9f9c9d20eb591d3e09
= <=>
2024-01-25 13:24:03 -0500

    Created game.py, deleted cell.py, developed logic for 1D array, TODO: complete logic

--- py/board.py
+++ py/board.py
@@ -1,61 +1,48 @@
-# Define the class
-# Define public methods
-    # Generate board (Width, Height, #Mines)
-    # Display (CSV, CMD)
-    # Regenerate board(Width, Height, #Mines)
-    # Reveal all mines
-
-# Private (__method__):
-    # clear -> clear the board
-    # Calculate nearby mines (set status)
-# ** 
 import random
-from cell import cell
 
 class board:
 
-    def __init__(self, *args): #This is a workaround since python does not allow constructor overloading.
-        self.height = 1
-        self.width = 1
-        self.mines = 1
-        self.cells = []
-        
-        if(len(args)==1 or len(args) == 2 or len(args) > 3): # Only accept 0 or arguments. Todo: Make sure all 3 args are Integers.
-            raise Exception("Please provide 0 or 3 arguments")
-        
-        elif(len(args) == 3):  #If there are 3 arguments do generate function.
-            if(args[2] < 1): #Make sure they provide at lease 1 mine.
-                raise Exception("There must be at least one mine")
-            self.generate(args[0], args[1], args[2])
-    
-    def generate(self, Height, Width, Mines):
-        if(Mines < 1): #Make sure they provide at lease 1 mine.
-                raise Exception("There must be at least one mine")
-        tempList = []
-        self.height = Height
-        self.width = Width
-        self.mines = Mines
-        for count in range(Height * Width):
-            self.cells.append(cell(0,0))
-            tempList.append(count)
+    def __init__(self, height, width, mines): #This is a workaround since python does not allow constructor overloading.
+        ## TODO: catch when width, height and mines are bad.
+        self.generate(height, width, mines)
 
-        for numMines in range(Mines):
-            x = random.choice(tempList)
-            print(x," ", tempList) #for debug purposes
-            self.cells[x].setValue(-1)
-            tempList.remove(x)
+    def generate(self, height, width, mines):
+        if(mines < 1): #Make sure they provide at lease 1 mine.
+                raise Exception("There must be at least one mine")
+        
+        self.height = height
+        self.width = width
+        self.mines = mines
+        self.cells = [0 for i in range(width*height)]
+        indexes = random.sample([i for i in range(width*height)], mines) # initiate to all 0
+        
+        for i in indexes:
+            self.init_mine(i)
+
+
+        print(self.cells) # TODO: delete me
+        print(indexes) ##
+
     
-    def display(self, Type):
+    def init_mine(self, idx):
+        self.cells[idx] = -1
+
+        change = [self.width + 1, self.width, self.width - 1, +1, -1, -self.width + 1, -self.width, -self.width - 1]
+        for adj in change:
+            print(idx + adj)
+            self.inc_cell(idx + adj, idx)
+
+    def inc_cell(self, idx, mine):
+        if ((idx > 0 and idx < self.width * self.height) or (idx % self.width <= 0 and idx % self.width < self.width)) and self.cells[idx] != -1:
+            self.cells[idx] += 1
+    
+    def display(self, Type = "csv"):
         if(Type.lower() == "csv"):
-            tempStr = ""
-            for count in range(self.height * self.width):
-                tempStr += str(self.cells[count].getValue()) + ","
-        return tempStr[:-1]
-
-
-
+            for i in range(self.height * self.width):
+                if i % self.width == 0:
+                    print()
+                print(self.cells[i], end=" ")
 
     
-b1 = board()
-b2 = board(2,2,3)
-print(b2.display("CSV"))
+b2 = board(5,5,1)
+b2.display()
